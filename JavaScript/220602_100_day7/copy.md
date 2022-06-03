ccbe8ce28be2617222c49ef0a5cb69a521e6b005
YANGDAYOUNG <jc1525@hanmail.net>
2022-06-03 14:51:38 +0900

    feat: click to copy with clipboardApi
    navigator.clipboard.writeText(text) 복사

---
+++ JavaScript/220602_100_day7/copy.md
@@ -0,0 +1,10 @@
+setSelectionRange(시작, 끝) /끝 포함안됨
+시작인덱스부터 끝 인덱스 직전까지 선택됨!! 
+https://developer.mozilla.org/en-US/docs/Web/API/HTMLInputElement/setSelectionRange
+
+
+
+코드 참고한 블로그 
+> 리액드와 Ts로 만들었음!!:  https://velog.io/@treejy/%EC%9B%B9%EC%97%90%EC%84%9C-%ED%81%B4%EB%A6%BD%EB%B3%B4%EB%93%9C%EC%97%90-%ED%85%8D%EC%8A%A4%ED%8A%B8-%EC%A0%80%EC%9E%A5%ED%95%98%EA%B8%B0-Clipboard-API-with-%EB%A6%AC%EC%95%A1%ED%8A%B8-TypeScript  
+> 이건 그냥 리액트인거 같은데, 딱히 리액트 문법인지를 모르겠 : https://kyounghwan01.github.io/blog/React/clipboard-copy/#%E1%84%89%E1%85%A5%E1%84%85%E1%85%A9%E1%86%AB  
+> 참고한 예시: https://goldenthumb.net/Clipboard-API
--- JavaScript/220602_100_day7/main.js
+++ JavaScript/220602_100_day7/main.js
@@ -1,17 +1,44 @@
-const btn = document.querySelector(".btn");
-const coupon = document.querySelector(".coupon");
+const copyBtnEl = document.querySelector(".btn");
+const couponStringEl = document.querySelector(".couponString");
 
-const copyText = (e) => {
-  e.preventDefault();
 
-  coupon.select();
-  coupon.setSelectionRange(0, 999999);
-  document.execCommand("copy");
 
-  btn.textContent = "Copied!!!";
-  setTimeout(() => {
-    btn.textContent = "Copy";
-  }, 3000);
-};
+const copyText = (text) => {
+  navigator.clipboard.writeText(text).then( () => {
+    //alert('클립보드에 복사되었습니다!')
+    copyBtnEl.textContent = "Copied!!!";
+    setTimeout(() => {
+      copyBtnEl.textContent = "Copy";
+    }, 3000);
+  },
+  () => {
+    alert('복사를 다시 시도해주세요!')
+  }
+  )
+}
 
-btn.addEventListener("click", copyText);
+
+copyBtnEl.addEventListener("click", (e) => {
+  e.preventDefault()// 왜인지는 모르겠지만 새로고침이 자꾸 일어나는거 같아서 일단 넣으니까 잘 됨..!! 위에 코드랑 비교해보고 싶다아아!!
+  console.log(couponStringEl.value)
+
+  copyText(couponStringEl.value)
+
+});
+
+// const copyText = (e) => {
+//   e.preventDefault();
+
+//   couponStringEl.select();
+//   couponStringEl.setSelectionRange(0, 999999);
+//   document.execCommand("copy");
+
+//   btn.textContent = "Copied!!!";
+//   setTimeout(() => {
+//     btn.textContent = "Copy";
+//   }, 3000);
+// };
+
+// btn.addEventListener("click", copyText);
+
+//execCommand는 더이상 사용하지 않는다... 브라우저에서 점점 지원을 안한다고...!! 
